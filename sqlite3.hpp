3f05ad34ffd04c4b6e5cd615e534ed39903815f5
Ciechowoj <keepitsimplesirius@gmail.com>
2014-08-23 13:45:32 +0200

    Add project with unit tests.\nAdd error definitions.

--- sqlite3.hpp
+++ sqlite3.hpp
@@ -1,24 +1,188 @@
 #include <stdexcept>
+#include <string>
+#include <functional>
 
 namespace sqlite3cpp {
+
+class database;
+class statement;
+
 namespace detail {
 
 void throw_exception(int, const char*);
 
+struct impl {
+	template <class T> static void*& get(T& x) {
+		return x._impl;
+	}
+
+	template <class T> static const void* get(const T& x) {
+		return x._impl;
+	}
+};
+
 }
 
 enum result_t {
-	ok,
 	row,
 	done
 };
 
+void clear_bindings(statement& statement);
+void bind(statement& statement, std::size_t index, double value);
+void bind(statement& statement, std::size_t index, long long value);
+void bind(statement& statement, std::size_t index, unsigned long long value);
+void bind(statement& statement, std::size_t index, const std::string& value);
+void bind(statement& statement, std::size_t index, const char* value);
+void bind(statement& statement, std::size_t index, const char* value, std::size_t bytes);
+void bind_null(statement& statement, std::size_t index);
+void bind_zeros(statement& statement, std::size_t index, std::size_t bytes);
+
+std::size_t bind_parameter_count(statement& statement);
+std::size_t bind_parameter_index(statement& statement, const char* name);
+std::size_t bind_parameter_index(statement& statement, const std::string& name);
+std::string bind_parameter_name_str(statement& statement, std::size_t index);
+const char* bind_parameter_name_cstr(statement& statement, std::size_t index);
+
+void close(database& database);
+
+double column_double(statement& statement, std::size_t index);
+signed char column_byte(statement& statement, std::size_t index);
+unsigned char column_ubyte(statement& statement, std::size_t index);
+short column_short(statement& statement, std::size_t index);
+unsigned short column_ushort(statement& statement, std::size_t index);
+int column_int(statement& statement, std::size_t index);
+unsigned int column_uint(statement& statement, std::size_t index);
+long column_long(statement& statement, std::size_t index);
+unsigned long column_ulong(statement& statement, std::size_t index);
+long long column_llong(statement& statement, std::size_t index);
+unsigned long long column_ullong(statement& statement, std::size_t index);
+std::string column_str(statement& statement, std::size_t index);
+const char* column_cstr(statement& statement, std::size_t index);
+std::size_t column_bytes(statement& statement, std::size_t index);
+
+template <class T> inline T column(statement& statement, std::size_t index) {
+	static_assert("Requested type T is invalid.", false);
+}
+
+template <> inline double column<double>(statement& statement, std::size_t index) {
+	return column_double(statement, index);
+}
+
+std::size_t column_count(statement& statement);
+std::string column_type_str(statement& statement, std::size_t index);
+const char* column_type_cstr(statement& statement, std::size_t index);
+std::string column_decltype_str(statement& statement, std::size_t index);
+const char* column_decltype_cstr(statement& statement, std::size_t index);
+std::string column_name_str(statement& statement, std::size_t index);
+const char* column_name_cstr(statement& statement, std::size_t index);
+std::string column_origin_name_str(statement& statement, std::size_t index);
+const char* column_origin_name_cstr(statement& statement, std::size_t index);
+std::string column_table_name_str(statement& statement, std::size_t index);
+const char* column_table_name_cstr(statement& statement, std::size_t index);
+std::string column_database_name_str(statement& statement, std::size_t index);
+const char* column_database_name_cstr(statement& statement, std::size_t index);
+
+statement prepare(database& database, std::string::const_iterator begin, std::string::const_iterator end, std::string::const_iterator& tail);
+statement prepare(database& database, const char* sql, const char*& tail);
+statement prepare(database& database, const char* sql, std::size_t bytes, const char*& tail);
+result_t step(statement& statement);
+void finalize(statement& statement);
+
+
+
+void exec(database& database, const char* sql);
+void exec(database& database, const char* sql, std::function<bool(int, const char* const*, const char* const*)> callback);
+
+
+
+class database {
+public:
+	database();
+	database(database&& that);
+	~database();
+	database& operator=(database&& that);
+	explicit operator bool();
+private:
+	void* _impl;
+	friend struct detail::impl;
+	database(const database&);
+	database& operator=(const database&);
+};
+
+class statement {
+public:
+	statement();
+	statement(statement&& that);
+	~statement();
+	statement& operator=(statement&& that);
+	explicit operator bool();
+private:
+	void* _impl;
+	friend struct detail::impl;
+	statement(const statement&);
+	statement& operator=(const statement&);
+};
+
+template <class T> inline std::pair<bool, T> exec(database& database, const char* sql, std::size_t bytes) {
+	static_assert("Requested type T is invalid.", false);
+}
+
+template <> inline std::pair<bool, long long> exec<long long>(database& database, const char* sql, std::size_t bytes) {
+	const char* itr = sql;
+	const char* end = sql + bytes;
+	sqlite3cpp::statement statement;
+	long long func_result = 0;
+	bool initialized = false;
+	while (itr != end) {
+		statement = prepare(database, itr, end - itr, itr);
+		result_t step_result = done;
+		do {
+			step_result = step(statement);
+			if (!initialized && column_count(statement) != 0) {
+				func_result = column<long long>(statement, 0);
+				initialized = true;
+			}
+		} while (step_result != done);
+		finalize(statement);
+	}
+
+	return std::make_pair(initialized, func_result);
+}
+
+template <> inline std::pair<bool, unsigned long long> exec<unsigned long long>(database& database, const char* sql, std::size_t bytes) {
+	const char* itr = sql;
+	const char* end = sql + bytes;
+	sqlite3cpp::statement statement;
+	long long func_result = 0;
+	bool initialized = false;
+	while (itr != end) {
+		statement = prepare(database, itr, end - itr, itr);
+		result_t step_result = done;
+		do {
+			step_result = step(statement);
+			if (!initialized && column_count(statement) != 0) {
+				func_result = column<long long>(statement, 0);
+				initialized = true;
+			}
+		} while (step_result != done);
+		finalize(statement);
+	}
+
+	return std::make_pair(initialized, func_result);
+}
+
+
+
+
 class sqlite_error : public std::runtime_error {
+protected:
 	sqlite_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
 class abort_error : public sqlite_error {
+protected:
 	abort_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
@@ -29,21 +193,25 @@ class auth_error : public sqlite_error {
 };
 
 class busy_error : public sqlite_error {
+protected:
 	busy_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
 class cantopen_error : public sqlite_error {
+protected:
 	cantopen_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
 class constraint_error : public sqlite_error {
+protected:
 	constraint_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
 class corrupt_error : public sqlite_error {
+protected:
 	corrupt_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
@@ -74,11 +242,13 @@ class interrupt_error : public sqlite_error {
 };
 
 class ioerr_error : public sqlite_error {
+protected:
 	ioerr_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
 class locked_error : public sqlite_error {
+protected:
 	locked_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
@@ -129,6 +299,7 @@ class range_error : public sqlite_error {
 };
 
 class readonly_error : public sqlite_error {
+protected:
 	readonly_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
@@ -153,87 +324,87 @@ class warning_error : public sqlite_error {
 	friend void detail::throw_exception(int, const char*);
 };
 
-class abort_rollback_error : public ioerr_error {
+class abort_rollback_error : public abort_error {
 	abort_rollback_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class busy_recovery_error : public ioerr_error {
+class busy_recovery_error : public busy_error {
 	busy_recovery_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class busy_snapshot_error : public ioerr_error {
+class busy_snapshot_error : public busy_error {
 	busy_snapshot_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class cantopen_convpath_error : public ioerr_error {
+class cantopen_convpath_error : public cantopen_error {
 	cantopen_convpath_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class cantopen_fullpath_error : public ioerr_error {
+class cantopen_fullpath_error : public cantopen_error {
 	cantopen_fullpath_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class cantopen_isdir_error : public ioerr_error {
+class cantopen_isdir_error : public cantopen_error {
 	cantopen_isdir_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_check_error : public ioerr_error {
+class constraint_check_error : public constraint_error {
 	constraint_check_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_commithook_error : public ioerr_error {
+class constraint_commithook_error : public constraint_error {
 	constraint_commithook_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_foreignkey_error : public ioerr_error {
+class constraint_foreignkey_error : public constraint_error {
 	constraint_foreignkey_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_function_error : public ioerr_error {
+class constraint_function_error : public constraint_error {
 	constraint_function_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_notnull_error : public ioerr_error {
+class constraint_notnull_error : public constraint_error {
 	constraint_notnull_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_primarykey_error : public ioerr_error {
+class constraint_primarykey_error : public constraint_error {
 	constraint_primarykey_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_rowid_error : public ioerr_error {
+class constraint_rowid_error : public constraint_error {
 	constraint_rowid_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_trigger_error : public ioerr_error {
+class constraint_trigger_error : public constraint_error {
 	constraint_trigger_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_unique_error : public ioerr_error {
+class constraint_unique_error : public constraint_error {
 	constraint_unique_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class constraint_vtab_error : public ioerr_error {
+class constraint_vtab_error : public constraint_error {
 	constraint_vtab_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class corrupt_vtab_error : public ioerr_error {
+class corrupt_vtab_error : public corrupt_error {
 	corrupt_vtab_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
@@ -353,29 +524,31 @@ class ioerr_write_error : public ioerr_error {
 	friend void detail::throw_exception(int, const char*);
 };
 
-class locked_sharedcache_error : public ioerr_error {
+class locked_sharedcache_error : public locked_error {
 	locked_sharedcache_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class readonly_cantlock_error : public ioerr_error {
+class readonly_cantlock_error : public readonly_error {
 	readonly_cantlock_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class readonly_dbmoved_error : public ioerr_error {
+class readonly_dbmoved_error : public readonly_error {
 	readonly_dbmoved_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class readonly_recovery_error : public ioerr_error {
+class readonly_recovery_error : public readonly_error {
 	readonly_recovery_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
-class readonly_rollback_error : public ioerr_error {
+class readonly_rollback_error : public readonly_error {
 	readonly_rollback_error(const char*);
 	friend void detail::throw_exception(int, const char*);
 };
 
 }
+
+namespace sqlite = sqlite3cpp;
